96ddc1513817af9b0c3c20c6b0fb7194bf7004fb
Patrick McCombs <pa.mccombs1@gmail.com>
2019-06-05 08:59:39 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,13 @@ class Post < ActiveRecord::Base
   has_many :categories, through: :post_categories
   has_many :comments
   has_many :users, through: :comments
+  
+  def categories_attributes=(categories)
+    categories.values.each do |attr|
+      category = Category.find_or_create_by(attr)
+      self.categories << category
+    end
+end
 
 
 end
